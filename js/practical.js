896cb9322a1bc8a7c178bfe32045598392b21f0f
Pasha <veniarskyi@gmail.com>
2023-01-14 19:57:18 +0200

    first commit

---
+++ js/practical.js
@@ -0,0 +1,124 @@
+//Перемінні
+/*const vehicleBodyWidth = 5000;
+const vehicleBodyLength = 4000;
+
+console.log("Ширина кузова авто:" + vehicleBodyLength + ' , довжина:' + vehicleBodyWidth);
+
+//['vruru', 'ecrvrev', 'vvuguegv'].map(a =>)
+
+//Request
+//data
+//response
+//Kebab-case
+//PascalCase
+
+//snake_case
+// UPPER_SNAKE_CASE
+
+let COLOR_RED = '#F00';
+
+
+//Масиви та об'єкти
+/*let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "5";
+
+const bool = true;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
+*/
+
+//Різниця між об'єктами та масивами
+/*const arr = ['a', 'b', 'c']; //Масив
+arr [10] = '3456';
+console.log(arr);
+
+const arrObj = {
+    a: 'a',
+    '1': 'b',
+    2: 'c'
+}
+const b = 'b';
+arrObj[b] = '1234';
+
+console.log(arrObj['b']);
+console.log(arrObj.b);
+//console.log(arr[1]);
+//console.log(arrObj[1]);
+
+//const obj ={a: 1, b: 2}; Об'єкт
+const obj = {
+    'Anna': 500,
+    'Alice': 800
+};
+
+//Вся інформація яку ми получили від користувача буде у вигляді строки
+
+//Взаємодія з користувачем на базовому рівні
+//alert('Hello!');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам є 18?", "18");
+//console.log(typeof(answer));
+
+const answer = [];
+
+answer[0] = prompt('Як вас звати?', '');
+answer[1] = prompt('Яка ваша фамілія?', '');
+answer[2] = prompt('Скільки вам років?', '');
+
+
+document.write(answer);
+
+//Інтерполяція (ES6)
+
+const category = 'toys';
+console.log('https://someurl.com/${category}/5');
+
+const user = "Ivan";
+
+alert(`Привіт, ${user}`);
+
+*/
+
+//Оператори JS
+/*console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr  = 10,
+decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+console.log(2 + 2 * 2 === 8);
+
+
+const isChecked = true;
+isClose = true;
+//console.log(isChecked && isClose);
+console.log(isChecked  || isClose);*/
